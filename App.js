cdef44d93a218b3cbd8603e241c67049b6965cc2
kirti sankhwar <kirtisankhwar2004@gmail.com>
2021-05-25 18:41:44 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Alert,
+} from "react-native";
+ 
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("./assets/Blue_Logo.png")} />
+ 
+      <StatusBar style="auto" />
+      <Text> allow sms permission</Text>
+<Text><Button
+        title="Turn on"
+        onPress={() => Alert.alert('Allow to send sms and view SMS messages')}
+      />
+      </Text>
+  
+        </View>
+  );
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 30,
+  },
+ 
+  image: {
+    marginBottom: 180,
+    width: 200,
+    height: 120,
+  },
+});
